aa6a8af388ced8a2abbba75d91088a12abae3b12
statusfailed <sf@statusfailed.com>
2019-12-31 18:19:12 +0000

    useful BitVec functions, partial + reverse derivs

--- src/RDA/BitVec.hs
+++ src/RDA/BitVec.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | Type-safe bitvectors
 -- Thanks: https://blog.jle.im/entry/fixed-length-vector-types-in-haskell.html
 module RDA.BitVec
   ( BitVec(..) -- ^ TODO: consider hiding constructors
   , bitVec
   , toBits
+  , convert
+  , append
+  , split
   ) where
 
 import Data.Proxy
@@ -41,8 +41,30 @@ toBits :: (KnownNat n, Bits t) => BitVec t n -> t
 toBits b@(BitVec x) = mask n .&. x
   where n = fromIntegral (natVal b) -- using bitvec as its own proxy.
 
+-- | Arbitrarily resize a bitvector, discarding most significant bits if m < n
+resize :: (KnownNat n, Bits t) => BitVec t n -> BitVec t m
+resize (BitVec x) = BitVec x
+
 -- | Change the underlying representation of a 'BitVec'
 -- /O(n)/.
 convert :: (KnownNat n, Bits a, Bits b) => BitVec a n -> BitVec b n
 convert a =
   foldl (\b i -> if testBit a i then setBit b i else b) zeroBits [0..finiteBitSize a]
+
+-- | Enlarge a vector
+expand :: (Bits t, KnownNat n, KnownNat m, n <= m) => BitVec t n -> BitVec t m
+expand (BitVec x) = BitVec x
+
+-- | Append two 'BitVec's.
+append :: (Bits t, KnownNat n, KnownNat m, m <= n + m, n <= n + m, KnownNat (n + m))
+  => BitVec t n -> BitVec t m -> BitVec t (n + m)
+append x y = expand x `xor` (expand y `shiftL` n) -- shiftL: we write numbers backwards!
+  where n = fromIntegral $ natVal x
+
+split :: (KnownNat n, KnownNat m, KnownNat (n + m), Bits t)
+  => BitVec t (n + m) -> (BitVec t n, BitVec t m)
+split xy = (x, y)
+  where
+    x = resize xy
+    y = resize (xy `shiftR` m)
+    m = fromIntegral (natVal y)
---
+++ src/RDA/Functions.hs
@@ -0,0 +1,46 @@
+module RDA.Functions
+  ( eval
+  , partial
+  , component
+  , rd1
+  ) where
+
+import RDA.BitVec
+import Data.Bits
+import Data.Proxy
+import GHC.TypeNats
+
+-- | Evaluate a truth table of @2^n@ bits as a function from @n@ bits to @1@ bit
+-- NOTE: this is technically unsafe, because we convert the second argument to
+-- an 'Int'.
+--
+-- Example: the truth table for NOT (truth table represented in binary as 10):
+--
+-- >>> let t = bitVec 1 :: BitVec Int (2^1) in eval t . bitVec <$> [0,1]
+-- [1, 0]
+eval :: (KnownNat n, Bits t) => BitVec t (2 ^ n) -> BitVec t n -> BitVec t 1
+eval t = convert . bitVec . testBit t . toBits . convert
+
+-- | The ith partial derivative at x for a function f
+-- TODO: @i@ can be larger than n - fix?
+partial :: (KnownNat n, Bits t)
+  => Int -> (BitVec t n -> BitVec t 1) -> BitVec t n -> BitVec t 1
+partial i f x = f x `xor` f (x `xor` e_i)
+  where e_i = bit i
+
+-- | @rd1 f@ is the reverse derivative of a function @f : n -> 1@ to its
+-- reverse derivative @rf : n + 1 -> n@, a function interpreted as saying,
+-- "at a given input and change of output, how should the input change to
+-- achieve the given change of output".
+-- NOTE: this is represented as a length @n@ tuple of functions, but we haven't
+-- encoded this in the type!
+-- TODO: better length-encoded list types?
+rd1 :: (KnownNat n, KnownNat (n+1), Bits t)
+  => Proxy n -> (BitVec t n -> BitVec t 1) -> [BitVec t (n + 1) -> BitVec t 1]
+rd1 n f = component f <$> [0..m-1]
+  where m = fromIntegral (natVal n)
+
+component :: (KnownNat n, KnownNat (n+1), Bits t)
+  => (BitVec t n -> BitVec t 1) -> Int -> BitVec t (n + 1) -> BitVec t 1
+component f i xy = partial i f x .&. y -- df/dx_i * y
+  where (x, y) = split xy
