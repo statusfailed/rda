92124735864020444d7a920bc530ae3f1022c10e
Paul Wilson <sf@statusfailed.com>
2020-01-03 10:43:26 +0000

    top level RDA module

--- src/RDA.hs
+++ src/RDA.hs
@@ -1,1 +1,11 @@
-module RDA where
+module RDA
+  ( module GHC.TypeNats
+  , module RDA.BitVec
+  , module RDA.Optimize
+  , module RDA.ReverseDerivative
+  ) where
+
+import GHC.TypeNats
+import RDA.BitVec
+import RDA.Optimize
+import RDA.ReverseDerivative
