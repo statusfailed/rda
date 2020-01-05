e83c4bf862a8df90fb523f2ad8162e01e001dc18
statusfailed <sf@statusfailed.com>
2020-01-05 14:15:47 +0000

    document parameters of rda function

--- src/RDA/Optimize.hs
+++ src/RDA/Optimize.hs
@@ -26,10 +26,10 @@ rdaUpdate params x y model = concatBits (Proxy :: Proxy p) dw
     dw = reverse $ fmap ($ append params dy) fs -- NOTE: reverse very important(!)
 
 rda :: (KnownNat p, KnownNat x, KnownNat (p+1), Bits t, p <= p + 1, 1 <= p + 1)
-  => [(BitVec t x, BitVec t 1)]
-  -> (BitVec t p -> BitVec t x -> BitVec t 1)
-  -> BitVec t p
-  -> [BitVec t p]
+  => [(BitVec t x, BitVec t 1)]  -- ^ A dataset of examples to learn
+  -> (BitVec t p -> BitVec t x -> BitVec t 1) -- ^ A model function from parameters and data to label
+  -> BitVec t p -- ^ Initial parameters (where to begin the search)
+  -> [BitVec t p] -- ^ Parameter search trace
 rda [] _ params = [params]
 rda ((x, y):dataset) model params = params : rda dataset model params'
   where
